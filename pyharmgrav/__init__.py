4758797f97738650da9d965464df0ac5b0d0136c
unknown <pongraczdaniel@edu.bme.hu>
2025-11-09 16:52:15 +0100

    fix imports

--- pyharmgrav/__init__.py
+++ pyharmgrav/__init__.py
@@ -1,11 +1,11 @@
 __all__ = ["point_sh_synthesis","grid_sh_synthesis","Ellipsoid",
            "geod2geoc", "read_shcs", "read_bhsc", "read_dat","read_mat"]
 
-from .pyharm_grav import point_sh_synthesis
-from .pyharm_grav import grid_sh_synthesis
+from .pyharm_grav_shs import point_sh_synthesis
+from .pyharm_grav_shs import grid_sh_synthesis
 from .normal_grav_field import Ellipsoid
-from .pyharm_grav_utils import geod2geoc
-from .pyharm_grav_utils import read_shcs
+from .pyharm_grav_shs_utils import geod2geoc
+from .pyharm_grav_shs_utils import read_shcs
 from .read_SH_coeffs import read_bhsc
 from .read_SH_coeffs import read_dat
 from .read_SH_coeffs import read_mat
